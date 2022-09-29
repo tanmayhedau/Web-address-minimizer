8cc20341625e41ac021023ed6e161560bb2f5f8b
bndu2002 <bndusharma2002@gmail.com>
2022-09-29 16:01:08 +0530

    2nd api(redirect)

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -63,18 +63,3 @@ const redirectURL = async function(req,res){
 module.exports = { shortURL , redirectURL}
 
 
-// router.get('/:code', async (req, res) => {
-//     try {
-//         // find a document match to the code in req.params.code
-//         const url = await Url.findOne({
-//             urlCode: req.params.code
-//         })
-//         if (url) {
-//             // when valid we perform a redirect
-//             return res.redirect(url.longUrl)
-//         } else {
-//             // else return a not found 404 status
-//             return res.status(404).json('No URL Found')
-//         }
-
-//     }
