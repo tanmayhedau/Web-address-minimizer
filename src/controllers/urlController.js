5d345e3c745eedcacdcf995ef40ee4799f3f9887
bndu2002 <bndusharma2002@gmail.com>
2022-09-29 17:00:06 +0530

    minor edits

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,13 +2,13 @@ const urlModel = require("../models/urlModel");
 const shortId = require('shortid');
 const validURL = require('valid-url');
 //const config = require('config')
-const { isPresent } = require('../validator/validator'); 
+const { isPresent } = require('../validator/validator');
 const { findOne } = require("../models/urlModel");
 
 
 const shortURL = async function (req, res) {
     try {
-        let { longUrl, shortUrl, urlCode } = req.body
+        let { longUrl, urlCode, shortUrl } = req.body
         let baseUrl = "http://localhost:3000"
 
         if (!isPresent(longUrl)) return res.status(400).send({ status: false, message: "long URL is mandatory" });
@@ -17,23 +17,28 @@ const shortURL = async function (req, res) {
 
         if (!validURL.isUri(baseUrl)) return res.status(400).send({ status: false, message: "baseUrl is invalid" });
 
-        if (!isPresent(urlCode) || !isPresent(shortUrl)){
+        if (!isPresent(urlCode) || !isPresent(shortUrl)) {
             req.body.urlCode = shortId.generate().toLowerCase();
             req.body.shortUrl = baseUrl + "/" + req.body.urlCode;
         }
 
-        
-
-        let findURL = await urlModel.findOne({ longUrl: longUrl });
+        let findURL = await urlModel.findOne({ longUrl: longUrl }).select({ _id: 0, __v: 0 })
 
         if (!findURL) {
 
-            let createURL = await urlModel.create(req.body)
+            let url = await urlModel.create(req.body)
+
+            let createURL = {
+                longUrl: url.longUrl,
+                shortUrl: url.shortUrl,
+                urlCode: url.urlCode
+            }
             return res.status(201).send({ status: true, message: "successfully shortend", data: createURL })
 
         }
 
-        return res.status(400).send({ status: true, message: "already exists" })
+        //409 : conflict with db
+        return res.status(409).send({ status: true, message: "long URL already exists" , data:findURL })
 
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message })
@@ -41,25 +46,27 @@ const shortURL = async function (req, res) {
 
 }
 
-const redirectURL = async function(req,res){
+
+
+const redirectURL = async function (req, res) {
     try {
 
         let urlCode = req.params.urlCode
 
-        if(!urlCode)return res.status(400).send({status:false , message : "provide urlCode"});
-        
-        let findUrlCode = await urlModel.findOne({urlCode : urlCode});
+        if (!urlCode) return res.status(400).send({ status: false, message: "provide urlCode" });
 
-        if(!findUrlCode)return res.status(400).send({status:false , message : "urlCode does not exist"}); 
-        
-        return res.status(302).redirect(findUrlCode.longUrl)
+        let findUrlCode = await urlModel.findOne({ urlCode: urlCode });
 
-        
+        if (!findUrlCode) return res.status(404).send({ status: false, message: "urlCode does not exist" });
+
+        return res.redirect(findUrlCode.longUrl)
+
+
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message })
     }
 }
 
-module.exports = { shortURL , redirectURL}
+module.exports = { shortURL, redirectURL }
 
 
