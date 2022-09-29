8de9620789a2afe2718538e48bffb9fc27d44906
tanmayhedau <tanmayhedau619@gmail.com>
2022-09-29 12:03:28 +0530

    project-4 starts

---
+++ src/controllers/urlController.js
@@ -0,0 +1,6 @@
+const urlModel = require("../models/urlModel")
+
+
+const createUrl = async function (req,res) {
+    
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const app = express();
+
+app.use(express.json());
+
+let url =
+  "mongodb+srv://tannmayhedau619:Tanmay%40619@cluster0.fw1xhuw.mongodb.net/group25Database";
+let port = process.env.PORT || 3000;
+
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(port, function () {
+  console.log("Express app running on port " + port);
+});
